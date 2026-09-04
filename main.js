77645d75811c1eb619e015c26c5c451a4dd1ecd8
rammyglitch-2 <Rammypius3@gmail.com>
2026-09-04 12:47:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const myFavouriteSubject = "mechanic";
+console.log("my favourite subject:", myFavouriteSubject);
+
+const colors = ["Red", "Black", "white", "Ash"];
+console.log("First color:", colors[0]);
+console.log("Number of colors:", colors.length);
+
+const favouriteMovie = {
+    title: "fast x",
+    director: "shuwan ya",
+    year: "2022",
+    rating: "87%"
+};
+console.log("Movie:", favouriteMovie.title);
