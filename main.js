b5e0202c10655e6488ace95832d5d172205a02cc
rammyglitch-2 <Rammypius3@gmail.com>
2026-09-04 12:53:17 +0100

    who

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ const favouriteMovie = {
     rating: "87%"
 };
 console.log("Movie:", favouriteMovie.title);
+
+const student = {
+    name: "abuhamad",
+    grades: [52, 56, 89],
+    gpa: 4.75
+};
+
+console.log("Student:", student.name);
+console.log("Grades:", student.grades);
+console.log("GPA:", student.gpa);
